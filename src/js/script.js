6b566ae7a9f882cab8d0b92410facedd70703b45
serg155alternate <serg1557733@i.ua>
2021-10-18 20:24:07 +0300

    section about and projects

--- src/js/script.js
+++ src/js/script.js
@@ -25,7 +25,8 @@ function hideOverlay() {
     document.body.style.overflow = "";
     overlay.style.display = 'none';
 }
-//menu toggle
+//menu header toggle 
+
 const menuItems = document.querySelectorAll('.header__menu-link'),
     menuParent = document.querySelector('.header__menu');
 
@@ -43,13 +44,45 @@ showItems(menuItems);
 
 
 menuParent.addEventListener('click', event => {
-    event.preventDefault();
+    event.preventDefault();//for tests
     const target = event.target;
     if (target && !target.classList.contains('header__menu-link_active')) {
         menuItems.forEach((item, i) => {
             if (target == item) {
                 hideItems();
                 showItems(menuItems, i);
+            }
+        });
+    }
+});
+
+
+//menu works toggle 
+
+const tabs= document.querySelectorAll('.works__menu-link'),
+    tabsParent = document.querySelector('.works__menu');
+
+function hideTabs() {
+    tabs.forEach(item => {
+        item.classList.remove('works__menu-link_active');
+    });
+}
+
+function showTabs(items, i = 0) {
+    items[i].classList.add('works__menu-link_active');
+}
+hideTabs();
+showTabs(tabs);
+
+tabsParent.addEventListener('click', event => {
+    event.preventDefault();//for tests
+    
+    const target = event.target;
+    if (target && !target.classList.contains('works__menu-link_active')) {
+        tabs.forEach((item, i) => {
+            if (target == item) {
+                hideTabs();
+                showTabs(tabs, i);
             }
         });
     }
